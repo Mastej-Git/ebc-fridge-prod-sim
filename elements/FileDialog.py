bafe2e021733d99fe9663b2b3b729763c88f3234
Michal Mastej <m.mastej1812@gmail.com>
2025-11-11 16:24:41 +0100

    Initialization of the control panel tab

---
+++ elements/FileDialog.py
@@ -0,0 +1,30 @@
+import os
+
+from PyQt5.QtWidgets import (
+    QWidget,
+    QLabel,
+    QFileDialog,
+)
+
+class FileDialog(QWidget):
+    def __init__(self, label: QLabel):
+        super().__init__()
+
+        self.label = label
+
+    def show_file_dialog(self):
+        home_dir = os.environ.get('HOME')
+
+        options = QFileDialog.Options()
+        read_file_name, _ = QFileDialog.getOpenFileName(
+            self, "Open File", "", 
+            "All Files (*);;Text Files (*.txt)", 
+            options=options
+        )
+        
+        if read_file_name.endswith(".json"):
+            self.label.setText(f'Wybrany plik: {read_file_name}')
+            return read_file_name
+        
+        self.label.setText(f'ZŁY FORMAT PLIKU! Oczekiwany format: .json\n\nWybrany plik: {read_file_name}')
+        return ""
---
+++ qt_classes/GUI.py
@@ -0,0 +1,121 @@
+from PyQt5.QtWidgets import (
+    QMainWindow, 
+    QTabWidget, 
+    QWidget, 
+    QVBoxLayout,
+    QHBoxLayout,
+    QLabel,
+    QGroupBox,
+    QFrame
+)
+from qt_classes.AnimatedButton import AnimatedButton
+from StyleSheet import StyleSheet
+from elements.FileDialog import FileDialog
+
+class GUI(QMainWindow):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.setWindowTitle("Tab Example")
+        self.setGeometry(100, 100, 800, 600)
+
+        central_widget = QFrame()
+        central_widget.setStyleSheet(StyleSheet.CentralWidget.value)
+        layout = QVBoxLayout(central_widget)
+
+        self.tabs = QTabWidget()
+        self.tabs.tabBar().setExpanding(True)
+        self.tabs.setStyleSheet(StyleSheet.Tab.value)
+
+        self.tab1 = QWidget()
+        self.tab2 = QWidget()
+        self.tab3 = QWidget()
+        self.tabs.addTab(self.tab1, "Tab 1")
+        self.tabs.addTab(self.tab2, "Tab 2")
+        self.tabs.addTab(self.tab3, "Tab 3")
+
+        self.create_tab_content()
+        layout.addWidget(self.tabs)
+        self.setCentralWidget(central_widget)
+
+    def create_tab_content(self):
+        sub_tab_widget = QTabWidget()
+        sub_tab_widget.setTabPosition(QTabWidget.West)
+        sub_tab_widget.setStyleSheet(StyleSheet.SubTab.value)
+
+        self.create_dial_tab()        
+        # layout1 = QVBoxLayout()
+        # self.tab1.setLayout(layout1)
+
+        layout2 = QVBoxLayout()
+        self.tab2.setLayout(layout2)
+
+        layout3 = QVBoxLayout()
+        self.tab3.setLayout(layout3)
+
+    def create_dial_tab(self):
+
+        layout1 = QVBoxLayout()
+
+        group_box1 = QGroupBox("Wczytaj konfigurację")
+        group_box1.setFixedHeight(200)
+
+        label = QLabel("Nie wybrano żadnego pliku")
+
+        hbox_layout = QHBoxLayout()
+        vbox_layout = QVBoxLayout()
+
+        read_file_button = AnimatedButton("Wczytaj")
+        # read_file_button.clicked.connect(self.pb_read_json)
+
+        self.file_dialog = FileDialog(label)
+
+        chose_file_button = AnimatedButton("Wybierz plik")
+        # chose_file_button.clicked.connect(self.pb_chose_file)
+
+        vbox_layout.addWidget(chose_file_button)
+        vbox_layout.addWidget(read_file_button)
+
+        hbox_layout.addWidget(label)
+        hbox_layout.addLayout(vbox_layout)
+
+        group_box2 = QGroupBox("Panel kontrolny")
+        group_box2.setFixedHeight(600)
+
+
+        hbox_layout1 = QHBoxLayout()
+
+        vbox_layout1 = QVBoxLayout()
+        body_counter_label = self.create_label(f"Wczytanych korpusów: 0")
+        production_counter_label = self.create_label(f"Korpusów w produkcji: 0")
+        finished_bodys_label = self.create_label(f"Wyprodukowanych korpusów: 0")
+        vbox_layout1.addWidget(body_counter_label)
+        vbox_layout1.addWidget(production_counter_label)
+        vbox_layout1.addWidget(finished_bodys_label)
+
+        vbox_layout2 = QVBoxLayout()
+        self.label_tmp = self.create_label("Brak wybranej ilości")
+        vbox_layout2.addWidget(self.label_tmp)
+        chose_value_button = AnimatedButton("Wybierz ilość")
+        # chose_value_button.clicked.connect(self.pb_input_dialog)
+        vbox_layout2.addWidget(chose_value_button)
+        produce_button = AnimatedButton("Produkuj")
+        # produce_button.clicked.connect(self.pb_produce)
+        vbox_layout2.addWidget(produce_button)
+
+        hbox_layout1.addLayout(vbox_layout1)
+        hbox_layout1.addLayout(vbox_layout2)
+
+        group_box2.setLayout(hbox_layout1)
+        group_box1.setLayout(hbox_layout)
+
+        layout1.addWidget(group_box2)
+        layout1.addWidget(group_box1)
+        self.tab1.setLayout(layout1)
+
+    def create_label(self, label_str: str, maximuxm_height: int = 70) -> QLabel:
+        label = QLabel(label_str)
+        label.setStyleSheet(StyleSheet.QLabel.value)
+        label.setMaximumHeight(maximuxm_height)
+        return label
+        
